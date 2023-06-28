72d36cdfe345cd0c5eaea4d6255a7a0d9795e5e0
dbgus1006 <dbgus1006@gmail.com>
2023-06-28 16:34:11 +0900

    Create BOJ_15650.cpp

---
+++ Silver/BOJ_15650/15650.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+static int N, M;
+static int arr[9] = { 0 };
+static bool visited[9] = { false };
+
+void DFS(int num, int cnt)
+{
+	if (cnt == M)
+	{
+		for (int i = 0; i < M; i++)
+		{
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = num; i <= N; i++)
+	{
+		if (!visited[i])
+		{
+			visited[i] = true;
+			arr[cnt] = i;
+			DFS(i + 1, cnt + 1);
+			visited[i] = false;
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+	cin >> N >> M;
+
+	DFS(1, 0);
+
+	return 0;
+}
